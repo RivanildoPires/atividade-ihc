289793ea2a34c9ad664169f65be19f3da38372de
Rivinha <126428857+RivanildoPires@users.noreply.github.com>
2024-09-19 13:07:33 -0300

    Add files via upload

--- Atividades IHC/Atividade 1/Atvd calculadora/calculadora.js
+++ Atividades IHC/Atividade 1/Atvd calculadora/calculadora.js
@@ -5,8 +5,20 @@ const button = document.getElementById("btnAdicionar")
 const resultado = document.getElementById("resultado")
 
 button.addEventListener("click", () =>{
-    const num1 = Number(input1.value)
-    const num2 = Number(input2.value)
+    let num1 = input1.value.trim();
+    let num2 = input2.value.trim();
+
+    if(num1 === ""){
+        num1 = 0;
+    }else{
+        num1 = Number(num1);
+    }
+
+    if(num2 === ""){
+        num2 = 0;
+    }else{
+        num2 = Number(num2);
+    }
 
     const op = operacao.value;
 
